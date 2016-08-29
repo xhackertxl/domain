92c82bbf48c494204e763148243a0203b9539e98
xhacekrtxl <xhackertxl@gmail.com>
2016-08-29 14:51:39 +0800

    test

--- backup.sh
+++ backup.sh
@@ -1,23 +1,28 @@
-tar cvpjf bin.tar.bz2  /bin
-tar cvpjf boot.tar.bz2 /boot
-tar cvpjf cdrom.tar.bz2 /cdrom
-tar cvpjf Desktop.tar.bz2 /Desktop
-tar cvpjf dev.tar.bz2 /dev
-tar cvpjf etc.tar.bz2 /etc
-tar cvpjf home.tar.bz2 /home
-tar cvpjf lib.tar.bz2 /lib
-tar cvpjf lib32.tar.bz2 /lib32
-tar cvpjf lib64.tar.bz2 /lib64
-tar cvpjf libx32.tar.bz2 /libx32
-tar cvpjf lost+found.tar.bz2 /lost+found
-tar cvpjf mnt.tar.bz2 /mnt
-tar cvpjf opt.tar.bz2 /opt
-tar cvpjf public.tar.bz2 /public
-tar cvpjf root.tar.bz2 /root
-tar cvpjf run.tar.bz2 /run
-tar cvpjf sbin.tar.bz2 /sbin
-tar cvpjf srv.tar.bz2 /srv
-tar cvpjf sys.tar.bz2 /sys
-tar cvpjf u01.tar.bz2 /u01
-tar cvpjf usr.tar.bz2 /usr
-tar cvpjf var.tar.bz2 /var
+	tar cvpjf bin.tar.bz2  /bin
+	tar cvpjf boot.tar.bz2 /boot
+	tar cvpjf cdrom.tar.bz2 /cdrom
+	tar cvpjf Desktop.tar.bz2 /Desktop
+	tar cvpjf dev.tar.bz2 /dev
+	tar cvpjf etc.tar.bz2 /etc
+	tar cvpjf home.tar.bz2 /home
+	tar cvpjf lib.tar.bz2 /lib
+	tar cvpjf lib32.tar.bz2 /lib32
+	tar cvpjf lib64.tar.bz2 /lib64
+	tar cvpjf libx32.tar.bz2 /libx32
+	tar cvpjf lost+found.tar.bz2 /lost+found
+	tar cvpjf mnt.tar.bz2 /mnt
+	tar cvpjf opt.tar.bz2 /opt
+	tar cvpjf public.tar.bz2 /public
+	tar cvpjf root.tar.bz2 /root
+	tar cvpjf run.tar.bz2 /run
+	tar cvpjf sbin.tar.bz2 /sbin
+	tar cvpjf srv.tar.bz2 /srv
+	tar cvpjf sys.tar.bz2 /sys
+	tar cvpjf u01.tar.bz2 /u01
+	tar cvpjf usr.tar.bz2 /usr
+	tar cvpjf var.tar.bz2 /var
+
+
+
+
+
